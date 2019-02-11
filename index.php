3c45141168c0ad603038445372f01435b36d25a7
Edwin <edwinltang@gmail.com>
2019-02-10 23:42:38 -0600

    Change look and feel of login screen

--- index.php
+++ index.php
@@ -8,21 +8,26 @@ if(isset($_SESSION['login_user'])){
 <!DOCTYPE html>
 <html>
 <head>
-<title>Sistema de empleos.</title>
-<link href="css/style.css" rel="stylesheet" type="text/css">
+<title>Requerimientos de Software</title>
+<link href="css/login.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div class="main">
-	<h1>Sistema de login</h1>
-	<div id="login">
-		<h2>Inicio de sesion</h2>
+	<div class="material" id="login">
+		<h2>Inicio de Sesion</h2>
 		<form action="" method="post">
-		<label>Nombre de usuario: </label>
-		<input id="name" name="username" placeholder="username" type="text">
-		<label>contraseña: </label>
-		<input id="password" name="password" placeholder="**********" type="password">
-		<input name="submit" type="submit" value=" ingresar ">
-		<span><?php echo $error; ?></span>
+			<div style="text-align:left">
+				<input type="text" id="name" name="username">
+				<label for="name">Nombre de usuario</label>
+			</div>
+			
+			<div style="text-align:left">
+				<input type="password" id="password" name="password">
+				<label for="password">Contrase&ntilde;a</label>
+			</div>
+			
+			<span style="color:red"><?php echo $error; ?></span>
+			<input name="submit" type="submit" value="Ingresar">
 		</form>
 	</div>
 </div>
